ee0cc4d9be3f0e1d0cff42b051baf4969ab8c586
K0K0-cod3s <etornam.koko@amalitech.com>
2025-03-04 11:34:08 +0000

    check the driver path

--- src/test/java/com/xyz/bank/utils/TestBase.java
+++ src/test/java/com/xyz/bank/utils/TestBase.java
@@ -1,31 +1,3 @@
-//package com.xyz.bank.utils;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-//public class TestBase {
-//    protected static WebDriver driver;
-//
-//    @BeforeAll
-//    public static void setUp() {
-//        WebDriverManager.chromedriver().setup();
-//        System.setProperty("webdriver.chrome.driver", "C:/chromedriver/chromedriver.exe");
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-//        System.out.println("hi");
-//    }
-//
-//    @AfterAll
-//    public static void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
-//}
 package com.xyz.bank.utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -33,30 +5,16 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.nio.file.Paths;
 
 public class TestBase {
     protected static WebDriver driver;
 
     @BeforeAll
     public static void setUp() {
-        // Automatically manage ChromeDriver
+        // WebDriverManager handles downloading and path setup
         WebDriverManager.chromedriver().setup();
 
-        // Get project directory dynamically
-        String projectPath = System.getProperty("user.dir");
-
-        // Set ChromeDriver path dynamically for different OS
-        String driverPath;
-        if (System.getProperty("os.name").toLowerCase().contains("win")) {
-            driverPath = Paths.get(projectPath, "drivers", "chromedriver.exe").toString();
-        } else {
-            driverPath = Paths.get(projectPath, "drivers", "chromedriver").toString();
-        }
-
-        System.setProperty("webdriver.chrome.driver", driverPath);
-
-        // Initialize WebDriver
+        // Initialize WebDriver without manually setting the driver path
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
